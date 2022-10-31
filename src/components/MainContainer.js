a5aad293e1e28dc41a4a2a6f47582c78a1ba687f
Jacob <jacobamelse@hotmail.com>
2022-10-30 23:09:54 -0500

    commit tree e453709a93b031d8aab0fbb3f73f3556bc52346f

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,74 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([])
+  const [filterBy, setFilterBy] = useState('Tech')
+  const [portfolio, setPortfolio] = useState([])
+  const [sortBy, setSortBy] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:3001/stocks')
+    .then(r => r.json())
+    .then((stock) => setStocks(stock))
+  }, [])
+  
+  function onChangeHandler(e){
+    setFilterBy(e)
+  }
+
+  function onStockBuy(stock){
+    if (!portfolio.includes(stock)){
+      setPortfolio([...portfolio, stock])
+    }
+  }
+
+  function onPortfolioStockClick(stock){
+    const removeFromPortfolio = portfolio.filter((target) => {
+      return stock.id !== target.id
+    })
+    setPortfolio(removeFromPortfolio)
+    }
+
+    function sortByClick(e){
+      setSortBy(e.target.value)
+    }
+
+    useEffect(() => {
+      if (sortBy === 'Alphabetically'){
+        const alphabeticalSort = [...stocks].sort((a, b) => {
+          const nameA = a.name.toUpperCase(); 
+          const nameB = b.name.toUpperCase(); 
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+            return 0;
+        })
+        setStocks(alphabeticalSort)
+      } else {
+        const priceSort = [...stocks].sort((a, b) => a.price - b.price);
+        setStocks(priceSort)
+      }
+},[ sortBy ])
+
+  const filteredByTypeStocks = stocks.filter((stock) => {
+      return stock.type === filterBy
+    })
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar onChangeHandler={onChangeHandler} sortByClick={sortByClick} sortBy={sortBy}/>
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={filteredByTypeStocks} handleClick={onStockBuy}/>
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer portfolio={portfolio} handleClick={onPortfolioStockClick}/>
         </div>
       </div>
     </div>
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({portfolio, handleClick}) {
+  
+
   return (
     <div>
       <h2>My Portfolio</h2>
       {
-        //render your portfolio stocks here
+     portfolio.map((stock) => {
+      return <Stock key={stock.id} stocks={stock} handleClick={(stock) => handleClick(stock)}/>
+     })
       }
     </div>
   );
